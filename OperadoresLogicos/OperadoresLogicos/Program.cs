a31e6e204bdc136e8d1bdb24de7eede9cbaf56f2
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2023-06-02 15:50:50 -0300

    identação

--- OperadoresLogicos/OperadoresLogicos/Program.cs
+++ OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -7,11 +7,11 @@ Console.WriteLine(c2);
 bool c3 = !(2 > 3) && 4 != 5;
 Console.WriteLine(c3);
 
-bool c4 = 10<5;
-bool c5 = c1 || c2 && c3; 
+bool c4 = 10 < 5;
+bool c5 = c1 || c2 && c3;
 Console.WriteLine(c5);
 
-if(c1 && c2 || c3)
+if (c1 && c2 || c3)
 {
     Console.WriteLine("Condicional");
 }
