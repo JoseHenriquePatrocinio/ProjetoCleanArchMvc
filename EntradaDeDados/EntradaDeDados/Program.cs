26cd805ddda7e71e46b01ff25ff3fb17b128ad51
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2023-06-02 15:16:24 -0300

    entradadados

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = Console.ReadLine();
+            Console.WriteLine("Saida:" + a);
+
+            string b = Console.ReadLine();
+            Console.WriteLine("Saida:" + b);
+
+            string c = Console.ReadLine();
+            Console.WriteLine("Saida:" + c);
+
+            string d = Console.ReadLine();
+            Console.WriteLine("Saida:" + d);
+
+            string s = Console.ReadLine();
+
+
+
+        }
+    }
+}
