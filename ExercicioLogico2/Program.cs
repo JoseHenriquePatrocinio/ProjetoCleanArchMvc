57777e8b9ea9185b56aa8d33bc0387ba6fc8a8c4
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-06-03 23:44:37 -0300

    Exercicio2Finalizado

---
+++ ExercicioLogico2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+using System.Globalization;
+
+namespace ExercicioLogico2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados produto: " + produto);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade a ser adicionada no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            produto.AddProdutoEstoque(qte);
+
+            Console.WriteLine("Dados produto: " + produto);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade a ser removida no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            produto.RemoveProdutoEstoque(qte);
+
+            Console.WriteLine("Dados produto: " + produto);
+
+
+        }
+    }
+}
