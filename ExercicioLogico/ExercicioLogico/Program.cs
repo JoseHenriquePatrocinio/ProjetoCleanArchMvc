0116c3732f291ff04699a908b422c43bc7d1dcf4
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-06-03 21:13:15 -0300

    AreaTriangulo

--- ExercicioLogico/ExercicioLogico/Program.cs
+++ ExercicioLogico/ExercicioLogico/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Globalization;
+
+double xa, xb, xc, ya, yb, yc;
+Console.WriteLine("Entre com as medidas do triangulo x: ");
+xa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+xb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+xc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com as medidas do triangulo y: ");
+ya = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+yb = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+yc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double p = (xa + xb + xc) / 2.0;
+double areax = Math.Sqrt(p * (p - xa) * (p - xb) * (p - xc));
+p = (ya + yb + yc) / 2.0;
+double areay = Math.Sqrt(p * (p - ya) * (p - yb) * (p - yc));
+
+Console.WriteLine("Area de x = " + areax.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Area de y = " + areay.ToString("F4", CultureInfo.InvariantCulture));
+
+if (areax > areay)
+{
+    Console.WriteLine("Maior area: X");
+}
+else
+{
+    Console.WriteLine("Maior area: Y");
+}
